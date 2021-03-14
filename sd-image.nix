3c490faabcff8324e31acb296d04274dd6707813
Leonard Pollak <leonardp@tr-host.de>
2021-03-14 11:49:31 +0100

    sd-image stuff

--- sd-image.nix
+++ sd-image.nix
@@ -10,21 +10,39 @@ in {
     <nixpkgs/nixos/modules/installer/cd-dvd/sd-image.nix>
   ];
 
-  boot.kernelPackages = pkgs.linuxPackagesNanopiR4S;
+  # building with emulation
+  nixpkgs.system = "aarch64-linux";
 
+  boot = {
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+    consoleLogLevel = lib.mkDefault 7;
+    kernelPackages = pkgs.linuxPackagesNanopiR4S;
+    kernelParams = ["cma=32M" "console=ttyS2,115200n8" "console=tty0"];
+  };
   sdImage = {
-    populateFirmwareCommands = let
-      configTxt = pkgs.writeText "config.txt" ''
-        # 'fixes' serial console
-        dtoverlay=disable-bt
-      '';
-      in ''
-        cp ${configTxt} config.txt
-      '';
+    # bzip2 compression takes loads of time with emulation, skip it.
+    compressImage = false;
+    populateFirmwareCommands = '''';
     populateRootCommands = ''
       mkdir -p ./files/boot
       ${extlinux-conf-builder} -t 3 -c ${config.system.build.toplevel} -d ./files/boot
     '';
   };
 
+  # Enable OpenSSH
+  services.sshd.enable = true;
+
+  # root autologin etc
+  users.users.root.password = "root";
+  services.openssh.permitRootLogin = lib.mkDefault "yes";
+  services.mingetty.autologinUser = lib.mkDefault "root";
+
+  #users.extraUsers.root.openssh.authorizedKeys.keys = [
+  #   ""
+  #];
+
+  networking.firewall.enable = false;
 }
