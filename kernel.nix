a5a1f9eccbf567b1e337ae47e0e64f4eb4f429e1
Leonard Pollak <leonardp@tr-host.de>
2021-03-23 14:58:21 +0100

    kernel rc7

--- kernel.nix
+++ kernel.nix
@@ -3,7 +3,7 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.10-rc1";
+  version = "5.10-rc7";
   extraMeta.branch = "5.10";
 
   # modDirVersion needs to be x.y.z, will always add .0
@@ -11,7 +11,7 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "1s4ywf93xrlkjjq3c4142qhmsvx3kl0xwkbc09ss6gln8lwqnga8";
+    sha256 = "1wi1lbqkwakg4zvl1dj1cnf7wngg31jmdmifkxw03vsfr17ik5cz";
   };
 
   # Should the testing kernels ever be built on Hydra?
