3f3c2eb05768686ea11716f9950151a3b82b7577
Leonard Pollak <leonardp@tr-host.de>
2021-07-21 19:31:29 +0200

    kernel 5.13-rc7 -> 5.13.4

--- kernel.nix
+++ kernel.nix
@@ -3,15 +3,18 @@
 with stdenv.lib;
 
 buildLinux (args // rec {
-  version = "5.13-rc7";
+  #version = "5.13-rc7";
+  version = "5.13.4";
   #extraMeta.branch = "5.10";
 
   # modDirVersion needs to be x.y.z, will always add .0
   modDirVersion = if (modDirVersionArg == null) then builtins.replaceStrings ["-"] [".0-"] version else modDirVersionArg;
 
   src = fetchurl {
-    url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
-    sha256 = "0p174gdlf0fj5xc7d56iddp2y09c4h94gk16x3psf30bpqg7v3b5";
+    #url = "https://git.kernel.org/torvalds/t/linux-${version}.tar.gz";
+    url = "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${version}.tar.xz";
+    #sha256 = "0p174gdlf0fj5xc7d56iddp2y09c4h94gk16x3psf30bpqg7v3b5";
+    sha256 = "0v3x1q1r0r8lyjg5hsj7yayfxqcgfj01p86ya4s0i9jaclpwv4ki";
   };
 
   # Should the testing kernels ever be built on Hydra?
